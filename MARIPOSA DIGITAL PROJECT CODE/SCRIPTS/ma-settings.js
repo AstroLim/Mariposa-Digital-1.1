c06016772a57b97880d3d0c2d223ca47fa0bf00f
LeasyLaige <lelouchiuvora243@gmail.com>
2025-03-16 18:33:15 +0800

    Main Admin Settings Functionality

--- MARIPOSA DIGITAL PROJECT CODE/SCRIPTS/ma-settings.js
+++ MARIPOSA DIGITAL PROJECT CODE/SCRIPTS/ma-settings.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signOut} from "firebase/auth";
-import { getDatabase} from "firebase/database";
+import { getAuth, signInWithEmailAndPassword, signOut, updatePassword, updateEmail} from "firebase/auth";
+import { getDatabase, update, ref} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -38,6 +38,15 @@ if (!user || !uid) {
 
 document.querySelector('.nav-user-container div').textContent = `${user.lastName}, ${user.firstName}`;
 
+document.querySelector('.input-show').addEventListener('click', () => {
+  document.querySelector('.input-password').type = document.querySelector('.input-password').type === 'password' ? 'text' : 'password';
+});
+document.querySelector('.input-username').placeholder = user.username;
+document.querySelector('.input-email').placeholder = user.email;
+document.querySelector('.input-firstName').placeholder = user.firstName;
+document.querySelector('.input-lastName').placeholder = user.lastName;
+document.querySelector('.input-phone').placeholder = user.phone;
+
 document.querySelector('.sidebar-logout').addEventListener('click', () => {
   signOut(auth).then(() => {
     alert('You are now logged out.');
@@ -48,4 +57,114 @@ document.querySelector('.sidebar-logout').addEventListener('click', () => {
     console.error(error);
     alert(`An error occurred while logging out. Please try again.`);
   });
+});
+
+document.querySelectorAll('.input-submit').forEach((input) => {
+  input.addEventListener('click', () => {
+    const header = input.closest('.settings-mainview-content-container').querySelector('h4');
+    const headerText = header.textContent;
+    console.log(`Header: ${headerText}`);
+    handleSubmit(headerText);
+  });
+
+  const handleSubmit = (headerText) => {
+    if (headerText === 'Username') {
+      update(ref(db, 'users/' + uid), {
+        username: document.querySelector('.input-username').value
+      }).then(() => {
+        alert(`Successfully changed your username to ${document.querySelector('.input-username').value}`);
+      }).catch((error) => {
+        console.error(error);
+        alert('An error occurred while submitting the form. Please try again');
+      });
+    } else if (headerText === 'Email') {
+      updateEmail(auth.currentUser, document.querySelector('.input-email').value).then(() => {
+        update(ref(db, 'users/' + uid), {
+          email: document.querySelector('.input-email').value
+        }).then(() => {
+          alert(`Successfully changed your ${headerText} to ${document.querySelector('.input-email').value}`);
+        }).catch((error) => {
+          console.error(error);
+          update(ref(db, 'users/' + uid), {
+            email: user.email
+          })
+          alert('An error occurred while submitting the form. Please try again');
+        });
+      }
+      ).catch((error) => {
+        console.error(error);
+        alert(error.replace('Firebase: ', ''));
+      });
+    } else if (headerText === 'Password') {
+      if (document.querySelector('.input-password').value.length < 6) {
+        alert('Please enter a new password with at least 6 characters.');
+        return;
+      } else {
+        showModal();  
+      }
+    } else if (headerText === 'First Name') {
+      update(ref(db, 'users/' + uid), {
+        firstName: document.querySelector('.input-firstName').value
+      }).then(() => {
+        alert(`Successfully changed your ${headerText} to ${document.querySelector('.input-firstName').value}`);
+      }).catch((error) => {
+        console.error(error);
+        alert('An error occurred while submitting the form. Please try again');
+      });
+    } else if (headerText === 'Last Name') {
+      update(ref(db, 'users/' + uid), {
+        lastName: document.querySelector('.input-lastName').value
+      }).then(() => {
+        alert(`Successfully changed your ${headerText} to ${document.querySelector('.input-username').value}`);
+      }).catch((error) => {
+        console.error(error);
+        alert('An error occurred while submitting the form. Please try again');
+      });
+    } else if (headerText === 'Phone Number') {
+      update(ref(db, 'users/' + uid), {
+        phone: document.querySelector('.input-phone').value
+      }).then(() => {
+        alert(`Successfully changed your ${headerText} to ${document.querySelector('.input-phone').value}`);
+      }).catch((error) => {
+        console.error(error);
+        alert('An error occurred while submitting the form. Please try again');
+      });
+    } else if (headerText === 'Profile Picture') {
+      // TODO: Profile Picture
+    } else {
+      alert('An error occurred while submitting the form. Please try again.');
+    }
+  }
+
+  const showModal = () => {
+    const modal = document.getElementById('passwordModal');
+    modal.style.display = 'block';
+
+    const cancelButton = document.getElementById('cancelButton');
+    const submitButton = document.getElementById('submitButton');
+
+    cancelButton.onclick = function() {
+      modal.style.display = 'none';
+    }
+
+    submitButton.onclick = function() {
+      const newPassword = document.querySelector('.input-password').value;
+      const verifyPassword = document.getElementById('passwordInput').value;
+
+      signInWithEmailAndPassword(auth, user.email, verifyPassword).then(() => {
+        alert('Password successfully verified!');
+        updatePassword(auth.currentUser, newPassword).then(() => {
+          alert('Password successfully updated!');
+          modal.style.display = 'none';
+        }).catch((error) => {
+          console.error(error);
+          alert(error.replace('Firebase: ', ''));
+        });
+      }).catch((error) => {
+        console.error(error);
+        alert(error.message.replace('Firebase: ', ''));
+        return;
+      });
+    }
+  }
 });
